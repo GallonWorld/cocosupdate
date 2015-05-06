0a17922b7fd9eff2958c9bc926bf30311383f731
Gallon <gsakd878@163.com>
2015-05-06 17:53:53 +0800

    测试
    Signed-off-by: Gallon <gsakd878@163.com>

---
+++ code/testngui/Assets/Script/testbutton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testbutton : MonoBehaviour {
+	protected bool switchFlag = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick()
+	{
+		if (this.switchFlag) {
+			print("true");
+			this.GetComponent<UISprite> ().spriteName = "20140512101542671";
+			this.GetComponent<UIButton> ().normalSprite = "20140512101542671";
+			this.switchFlag = false;
+		} else {
+			print("false");
+			this.GetComponent<UISprite> ().spriteName = "20140512101531109";
+			this.GetComponent<UIButton> ().normalSprite = "20140512101531109";
+			this.switchFlag = true;
+		}
+	}
+}
